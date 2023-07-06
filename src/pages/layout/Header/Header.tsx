b8287597b0002f12f25caca4fca039b0995e4531
camewell <130561684+camewell071@users.noreply.github.com>
2023-07-06 18:29:46 +0700

    chore: add ui winner

--- src/pages/layout/Header/Header.tsx
+++ src/pages/layout/Header/Header.tsx
@@ -6,7 +6,7 @@ import { Row } from '@/components/Row';
 const Header = () => {
   return (
     <S.Container>
-      <Row gap="60px" padding="12px 0">
+      <Row gap="60px" padding="24px 0">
         <LogoIcon />
         <ButtonLogin />
       </Row>
